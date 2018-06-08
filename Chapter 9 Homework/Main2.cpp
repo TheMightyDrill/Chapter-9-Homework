9e05e7e6060bc20aebd28c9b22a0cdc690b0187a
CS550 <CS550@T550-A5479>
2018-06-08 10:20:14 -0700

    HashTable

--- Chapter 9 Homework/Main2.cpp
+++ Chapter 9 Homework/Main2.cpp
@@ -63,16 +63,27 @@ public:
 	}
 
 	//overloading comparison operator
-	bool operator=(State &otherState) {
-
-		if (this->name == otherState.getName()) {
+	bool operator==(State other)
+	{
+		if (this->name == other.getName())
 			return true;
-		}
-
 		return false;
 	}
 
+	bool operator!=(State other)
+	{
+		if (this->name != other.getName())
+			return true;
+		return false;
+	}
 };
+
+ostream& operator<<(ostream& os, State& dt)
+{
+	os << dt.getCapital() << ' ' << dt.getCapital() << endl;
+	return os;
+}
+
 
 //contains and manipulates state data
 class StateData:public string {
@@ -129,17 +140,233 @@ public:
 
 };
 
-class HashT {
+//from class
 
+template <class elemType>
+class hashT
+{
+public:
+
+	//overloading operators
+	bool operator=(State &otherState) {
+
+		if (this->name == otherState.getName()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	bool operator!=(State &otherState) {
+		if (this->name == otherState.getName()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	void insert(int hashIndex, const elemType& rec);
+	//Function to insert an item in the hash table. The first
+	//parameter specifies the initial hash index of the item to 
+	//be inserted. The item to be inserted is specified by the 
+	//parameter rec.
+	//Postcondition: If an empty position is found in the hash
+	//   table, rec is inserted and the length is incremented by
+	//   one; otherwise, an appropriate error message is
+	//   displayed.
+
+	void search(int& hashIndex, const elemType& rec, bool& found) const;
+	//Function to determine whether the item specified by the
+	//parameter rec is in the hash table. The parameter hashIndex
+	//specifies the initial hash index of rec.
+	//Postcondition: If rec is found, found is set to true and
+	//   hashIndex specifies the position where rec is found;
+	//   otherwise, found is set to false.
+
+	bool isItemAtEqual(int hashIndex, const elemType& rec) const;
+	//Function to determine whether the item specified by the
+	//parameter rec is the same as the item in the hash table 
+	//at position hashIndex.
+	//Postcondition: Returns true if HTable[hashIndex] == rec;
+	//   otherwise, returns false.
+
+	void retrieve(int hashIndex, elemType& rec) const;
+	//Function to retrieve the item at position hashIndex.
+	//Postcondition: If the table has an item at position
+	//   hashIndex, it is copied into rec.
+
+	void remove(int hashIndex, const elemType& rec);
+	//Function to remove an item from the hash table.
+	//Postcondition: Given the initial hashIndex, if rec is found
+	//   in the table it is removed; otherwise, an appropriate
+	//   error message is displayed.
+
+	void print() const;
+	//Function to output the data.
+
+	hashT(int size = 101);
+	//constructor
+	//Postcondition: Create the arrays HTTable and indexStatusList;
+	//   initialize the array indexStatusList to 0; length = 0;
+	//   HTSize = size; and the default array size is 101.
+
+	~hashT();
+	//destructor
+	//Postcondition: Array HTable and indexStatusList are deleted.
+	int hashFunc(string name)
+	{
+		int i, sum;
+		int len;
+		i = 0;
+		sum = 0;
+		len = name.length();
+
+		for (int k = 0; k < 15 - len; k++)
+		{
+			name = name + ' ';//increase name to 15;
+		}
+
+		for (int k = 0; k < 5; k++)
+		{
+			sum = sum + ((int)name[i]) * 128 * 128 + ((int)name[i + 1])
+				* 128 + (int)name[i + 2];
+			i += 3;
+		}
+
+		return sum%HTSize;
+
+	}
+private:
+	elemType *HTable;   //pointer to the hash table
+	int *indexStatusList;  //pointer to the array indicating the
+						   //status of a position in the hash table
+	int length;    //number of items in the hash table
+	int HTSize;    //maximum size of the hash table
 };
+
+template <class elemType>
+void hashT<elemType>::insert(int hashIndex, const elemType& rec)
+{
+	int pCount;
+	int inc;
+
+	pCount = 0;
+	inc = 1;
+
+	while (indexStatusList[hashIndex] == 1
+		&& HTable[hashIndex] != rec
+		&& pCount < HTSize / 2)
+	{
+		pCount++;
+		hashIndex = (hashIndex + inc) % HTSize;
+		inc = inc + 2;
+		cout << "\nCOLLISION\n";
+	}
+
+
+	if (indexStatusList[hashIndex] != 1)
+	{
+		HTable[hashIndex] = rec;
+		indexStatusList[hashIndex] = 1;
+		length++;
+	}
+	else
+		if (HTable[hashIndex] == rec)
+			cerr << "Error: No duplicates are allowed." << endl;
+		else
+			cerr << "Error: The table is full. "
+			<< "Unable to resolve the collision." << endl;
+}
+
+template <class elemType>
+void hashT<elemType>::search(int& hashIndex, const elemType& rec, bool& found) const
+{
+	cout << "See Programming Exercise 7 at the end of Chapter 9." << endl;
+}
+
+template <class elemType>
+bool hashT<elemType>::isItemAtEqual(int hashIndex, const elemType& rec) const
+{
+	cout << "See Programming Exercise 7 at the end of Chapter 9." << endl;
+}
+
+template <class elemType>
+void hashT<elemType>::retrieve(int hashIndex, elemType& rec) const
+{
+	cout << "See Programming Exercise 7 at the end of Chapter 9." << endl;
+}
+
+template <class elemType>
+void hashT<elemType>::remove(int hashIndex, const elemType& rec)
+{
+	cout << "See Programming Exercise 7 at the end of Chapter 9." << endl;
+}
+
+template <class elemType>
+void hashT<elemType>::print() const
+{
+	for (int i = 0; i < this->HTSize; i++)
+	{
+		if (indexStatusList[i] == 1)
+		{
+			cout << HTable[i];
+		}
+	}
+}
+
+template <class elemType>
+hashT<elemType>::hashT(int size)
+{
+	HTSize = size;
+	HTable = new elemType[HTSize];   //pointer to the hash table
+	indexStatusList = new int[HTSize];  //pointer to the array indicating the
+	length = 0;    //number of items in the hash table
+
+	for (int i = 0; i < HTSize; i++)
+	{
+		indexStatusList[i] = 0;
+	}
+}
+
+template <class elemType>
+hashT<elemType>::~hashT()
+{
+	delete HTable;
+	delete indexStatusList;
+}
 
 int main() {
 
-	StateData data;
+	
+	State am;
+	State be;
+	State dep;
 
-	data.createSomeStates();
+	am.setName("Washington");
+	am.setCapital("Olympia");
+	am.setArea(50);
+	am.setOrderOfAdmission(39);
+	am.setYearOfSubmission(1890);
 
-	data.printStates();
+	be.setName("Arkansas");
+	be.setCapital("Little Rock");
+	be.setArea(50);
+	be.setOrderOfAdmission(39);
+	be.setYearOfSubmission(1890);
+
+	dep.setName("Mississippi");
+	dep.setCapital("Jackson");
+	dep.setArea(50);
+	dep.setOrderOfAdmission(39);
+	dep.setYearOfSubmission(1890);
+
+	hashT<State> hTable(3);
+
+	hTable.insert(hTable.hashFunc(am.getName()), am);
+	hTable.insert(hTable.hashFunc(be.getName()), be);
+	hTable.insert(hTable.hashFunc(dep.getName()), dep);
+
+	hTable.print();
 
 	system("pause");
 	return 0;
